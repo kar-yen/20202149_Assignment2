d9dec1224131c2b84a276404f24fda613cceaa2a
kar-yen <kar.yap@ucdconnect.ie>
2021-04-19 18:29:34 +0800

    Main .c file

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "initialization.h"
+#include "playGame.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    player p1 = {"Player 1", EMPTY, 0};
+    player p2 = {"Player 2", EMPTY, 0};
+
+    disc board[BOARD_SIZE][BOARD_SIZE];
+
+    initializePlayer(&p1, &p2);
+    initializeBoard(board);
+    printBoard(board, p1, p2);
+
+    takeTurn(board, &p1, &p2);
+
     return 0;
 }
