63c51a1fae81c0100bb1d8a88fc4fa0a48ca4ec1
kar-yen <kar.yap@ucdconnect.ie>
2021-04-30 15:32:31 +0800

    .h file to play the game

--- playGame.h
+++ playGame.h
@@ -2,5 +2,6 @@
 #define ASSIGNMENT2_PLAYGAME_H
 
 void takeTurn(disc board[][BOARD_SIZE], player *p1, player *p2);
+void getMove(disc board[][BOARD_SIZE], player *current, player *opponent, int num, int *pass);
 
 #endif //ASSIGNMENT2_PLAYGAME_H
