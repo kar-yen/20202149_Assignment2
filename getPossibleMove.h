cbbf89f32aed299dd1fa61eae91470184f0bfeb3
kar-yen <kar.yap@ucdconnect.ie>
2021-04-30 15:33:26 +0800

    .h file to find possible move of current player

---
+++ getPossibleMove.h
@@ -0,0 +1,6 @@
+#ifndef ASSIGNMENT2_GETPOSSIBLEMOVE_H
+#define ASSIGNMENT2_GETPOSSIBLEMOVE_H
+
+void findMove(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+
+#endif //ASSIGNMENT2_GETPOSSIBLEMOVE_H
