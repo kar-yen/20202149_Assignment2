d1463c115e2daa8dc35113f4c2c9b39e61669547
kar-yen <kar.yap@ucdconnect.ie>
2021-04-30 15:34:01 +0800

    .h file to update board and score after each turn

---
+++ updateBoard.h
@@ -0,0 +1,6 @@
+#ifndef ASSIGNMENT2_UPDATEBOARD_H
+#define ASSIGNMENT2_UPDATEBOARD_H
+
+void mainUpdate(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+
+#endif //ASSIGNMENT2_UPDATEBOARD_H
