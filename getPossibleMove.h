6e2c87e1d8bf3e2c683e95071d46a51617b547c8
kar-yen <kar.yap@ucdconnect.ie>
2021-05-02 01:43:04 +0800

    .h file to find possible move of current player

--- getPossibleMove.h
+++ getPossibleMove.h
@@ -2,5 +2,13 @@
 #define ASSIGNMENT2_GETPOSSIBLEMOVE_H
 
 void findMove(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findNorthWest(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findNorth(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findNorthEast(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findWest(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findEast(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findSouthWest(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findSouth(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
+void findSouthEast(disc board[][BOARD_SIZE], possibleMovePtr *sPtr, player opponent, int row, int column);
 
 #endif //ASSIGNMENT2_GETPOSSIBLEMOVE_H
