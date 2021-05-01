d9bac060dc99e9e09e20f9556615416d8797c3e1
kar-yen <kar.yap@ucdconnect.ie>
2021-05-02 01:41:44 +0800

    .h file to update board and score after each turn

--- updateBoard.h
+++ updateBoard.h
@@ -2,5 +2,14 @@
 #define ASSIGNMENT2_UPDATEBOARD_H
 
 void mainUpdate(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateNorthWest(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateNorth(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateNorthEast(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateWest(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateEast(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateSouthWest(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateSouth(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+void updateSouthEast(disc board[][BOARD_SIZE], player *current, player *opponent, int row, int column);
+
 
 #endif //ASSIGNMENT2_UPDATEBOARD_H
