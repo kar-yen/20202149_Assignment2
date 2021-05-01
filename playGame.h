755f32fdfa92975419c1b95e15380f74c5af1def
kar-yen <kar.yap@ucdconnect.ie>
2021-05-02 01:42:07 +0800

    .h file to play the game

--- playGame.h
+++ playGame.h
@@ -3,5 +3,10 @@
 
 void takeTurn(disc board[][BOARD_SIZE], player *p1, player *p2);
 void getMove(disc board[][BOARD_SIZE], player *current, player *opponent, int num, int *pass);
+void printList(possibleMovePtr cPtr);
+int isEmpty(possibleMovePtr sPtr);
+void delete(possibleMovePtr *cPtr);
+int findInList(possibleMovePtr *sPtr, int r, int c);
+void placeMove(disc board[][BOARD_SIZE], player *current, player *opponent, const char move[], int r, int c, int *pass, int num, possibleMovePtr *sPtr);
 
 #endif //ASSIGNMENT2_PLAYGAME_H
