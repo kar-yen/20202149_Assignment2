f394498d7cc2bf83ae4e1fee0950e8d053b833c0
kar-yen <kar.yap@ucdconnect.ie>
2021-05-02 01:47:27 +0800

    Main .c file

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <time.h>
 #include "initialization.h"
 #include "playGame.h"
@@ -15,16 +14,13 @@ int main()
     time_t startTime; // Variable to hold start time of game
     time_t endTime; // Variable to hold end time of game
 
-
     time(&startTime); // Store current time as start time
-    //printf("%s\n", ctime(&startTime));
 
     mainInitialize(board, &p1, &p2); // Initialize player and board
+
     takeTurn(board, &p1, &p2); // Manage the game turn between two players
 
     time(&endTime); // Store current time as end time
-    //printf("%s\n", ctime(&endTime));
-    //printf("%.2lf\n", difftime(endTime, startTime));
 
     mainEnd(board, p1, p2, startTime, endTime); // Print out results when game is over
 
