f39f9aa195cd8120ab3c442576b27014f25efa0d
kar-yen <kar.yap@ucdconnect.ie>
2021-05-04 22:48:29 +0800

    .h file to manage end of game

--- endGame.h
+++ endGame.h
@@ -1,5 +1,6 @@
 #ifndef ASSIGNMENT2_ENDGAME_H
 #define ASSIGNMENT2_ENDGAME_H
+#include <stdio.h>
 
 void mainEnd(disc board[][BOARD_SIZE], player p1, player p2, time_t t1, time_t t2);
 void createFile(disc board[][BOARD_SIZE], player p1, player p2, time_t t1, time_t t2, int min, int sec);
